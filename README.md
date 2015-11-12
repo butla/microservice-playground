9a361adf9df5673abdf567c4b2a6192ce6187b77
Michal Bultrowicz <michalbultrowicz@gmail.com>
2015-11-12 21:52:42 +0100

    A draft of aiohttp microservice.

--- README.md
+++ README.md
@@ -2,5 +2,5 @@ microservice-playground
 =======================
 Collection of microservice examples in different technologies, performance comparisons, proofs of concepts (PoCs), etc.
 
-I'm focusing on Falcon and Tornado.
-
+Most is done for Falcon (as a sync solution) and Tornado (as an async solution).
+The focus on Tornado will most probably switch to aiohttp on API-Hour.
---
+++ aiohttp_app/app.py
@@ -0,0 +1,34 @@
+import asyncio
+from aiohttp import web
+
+
+@asyncio.coroutine
+def handle(request):
+    # schedule something to run in the background
+    asyncio.get_event_loop().create_task(background_stuff())
+    
+    name = request.match_info.get('name', "Anonymous")
+    text = "Hello, " + name
+    return web.Response(body=text.encode('utf-8'))
+
+
+@asyncio.coroutine
+def init(loop):
+    app = web.Application(loop=loop)
+    app.router.add_route('GET', '/', handle)
+    app.router.add_route('GET', '/{name}', handle)
+
+    srv = yield from loop.create_server(app.make_handler(), '127.0.0.1', 8080)
+    print("Server started at http://127.0.0.1:8080")
+    return srv
+
+
+@asyncio.coroutine
+def background_stuff():
+    yield from asyncio.sleep(5)
+    print("5 seconds passed since a call...")
+
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(init(loop))
+loop.run_forever()
