92c8a1d458c078fcde980eac7f8c951e75766d10
Michal Bultrowicz <michalbultrowicz@gmail.com>
2017-04-14 23:39:56 +0200

    Standard run scripts for aiohttp and go

---
+++ go_app/run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+root_dir=$(dirname $0)
+executable=${root_dir}/app
+
+if [ ! -a ${executable} ]; then
+    go build -o ${executable} ${root_dir}/app.go
+fi
+
+${executable}
+
---
+++ simple_perf_test.sh
@@ -0,0 +1,1 @@
+wrk -c 8 -t 2 -d 30s http://localhost:9090
