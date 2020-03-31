e0f17d664524c9fa597b3b7e710c0af770ec31d6
Michal Bultrowicz <michal.bultrowicz@gmail.com>
2020-03-31 22:48:18 +0200

    Responder app update for benchmark.

--- responder_app/app.py
+++ responder_app/app.py
@@ -1,3 +1,5 @@
+import logging
+
 import responder
 
 api = responder.API()
@@ -6,9 +8,17 @@ api = responder.API()
 class SampleResource:
     async def on_get(self, req, resp, *, who):
         who = who if who else 'world'
-        resp.media = {'hello': who}
+        resp.text = {'hello': who}
+
+
+class BenchmarkResource:
+    async def on_get(self, req, resp):
+        resp.text = 'Hello, world'
 
 
 if __name__ == '__main__':
     api.add_route('/{who}', SampleResource)
-    api.run(port=9090)
+    api.add_route('/', BenchmarkResource)
+    api.run(port=9090, log_level=logging.WARNING)
+    # with logging
+    # api.run(port=9090)
