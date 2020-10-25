e4b9bce8e70609d0f85f1e59f8f482ac3bd6a44f
Michal Bultrowicz <michal.bultrowicz@gmail.com>
2020-10-25 21:36:06 +0100

    Playing around with FastAPI.

--- fastapi_app/app/app.py
+++ fastapi_app/app/app.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+import uvicorn
 
 app = FastAPI()
 
@@ -14,3 +15,14 @@ def with_params(path_param: int, query_param: str):
         "path": path_param,
         "query_param": query_param,
     }
+
+
+# # TODO after https://github.com/encode/uvicorn/issues/492 is solved use the factory app
+# async def asgi_app(scope, receive, send):
+#     app = FastAPI()
+#     app.add_route('/', hello, methods=['get'])
+#     app.add_api_route('/', hello, methods=['get'])
+#     await app(scope, receive, send)
+
+
+# TODO setup pylint, mypy
